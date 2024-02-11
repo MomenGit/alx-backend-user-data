20b2cc4d40667c400026654bcdd4fe4b30641076
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-11 22:21:58 +0200

    Add is_valid function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_pw = bcrypt.hashpw(pw_bytes, salt)
     return hashed_pw
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password matches the hashed password"""
+    pw_bytes = password.encode()
+    valid = bcrypt.checkpw(pw_bytes, hashed_password)
+    return valid
