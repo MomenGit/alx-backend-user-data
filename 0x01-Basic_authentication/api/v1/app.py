22a1d8b297eaa5bba6823a8fd0c9d66524a8809f
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-12 21:49:56 +0200

    Move auth_type outside of main scope

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,15 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
+auth_type = getenv("AUTH_TYPE")
+
+if auth_type == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -54,13 +63,5 @@ def before_req():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    auth_type = getenv("AUTH_TYPE")
-
-    if auth_type == "auth":
-        from api.v1.auth.auth import Auth
-        auth = Auth()
-    if auth_type == "basic_auth":
-        from api.v1.auth.basic_auth import BasicAuth
-        auth = BasicAuth()
 
     app.run(host=host, port=port)
