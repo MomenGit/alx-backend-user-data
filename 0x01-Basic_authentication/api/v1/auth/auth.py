dc35f8148cd79586e9d24299c3f75aae43da7ac0
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-12 14:48:06 +0200

    Write require_auth function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,13 +8,23 @@ class Auth:
     """A class to manage the API authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """"""
+        """
+        Returns True if the path is not in the list of strings excluded_paths
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if not path.endswith('/'):
+            path = path+'/'
+        if path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
         """"""
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """"""
+    def current_user(self, request=None):
+        """-> TypeVar('User'):
+        """
         return None
