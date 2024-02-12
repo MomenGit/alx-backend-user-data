606f4ece80c130b4b32294d5bb3d619b2e73912f
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-12 21:58:46 +0200

    Change current_user method annotation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -72,7 +72,7 @@ class BasicAuth(Auth):
 
         return None
 
-    def current_user(self, request=None) -> TypeVar['User']:
+    def current_user(self, request=None) -> TypeVar('User'):
         """Overloads Auth and retrieves the User instance for a request
         """
         auth_header = self.authorization_header(request)
