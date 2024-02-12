187ba37ab0a1578d5bc7c4c05e40e116663f8eaf
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-12 22:12:43 +0200

    Modify user_object_from_credentials method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -57,18 +57,18 @@ class BasicAuth(Auth):
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """Returns the User instance based on his email and password
         """
-        if user_email is None or user_pwd is None:
+        if user_email is None or not isinstance(user_email, str):
             return None
-        if type(user_email) is not str or type(user_pwd) is not str:
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
         users = User.search({"email": user_email})
         if len(users) == 0:
             return None
 
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
+        user = users[0]
+        if user.is_valid_password(user_pwd):
+            return user
 
         return None
 
