b3f2280266fee6b02121991ef7108f54aed3aaad
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-21 14:04:33 +0200

    Implement user registration route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,17 +2,30 @@
 """
 Route Module for the API
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from auth import Auth
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+AUTH = Auth()
 
 
 @app.route("/", strict_slashes=False)
 def index():
     """Return a JSON payload"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    """User Registration route"""
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError as err:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
