c527bd7d5c279a6e67f2b68eeb616f3dff1bdd01
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-21 13:30:17 +0200

    Add Auth class and implement register_user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 """Authentication Module"""
 import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Saves a new user"""
+        user = None
+
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception as err:
+            pass
+
+        if user is not None:
+            raise ValueError(f"User {email} already exists")
+
+        hashed_pwd = _hash_password(password)
+        new_user = self._db.add_user(email, hashed_pwd)
+        return new_user
 
 
 def _hash_password(password: str) -> bytes:
