17c6b3967d8ce41e8b82bd6fe98d74218ccefaea
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-22 12:10:58 +0200

    Implement Auth.valid_login method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,6 +28,18 @@ class Auth:
         new_user = self._db.add_user(email, hashed_pwd)
         return new_user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks that the user exists and that the password is correct"""
+        try:
+            user = self._db.find_user_by(email=email)
+            encoded_pwd = password.encode('utf-8')
+
+            return bcrypt.checkpw(encoded_pwd, user.hashed_password)
+        except Exception as err:
+            pass
+
+        return False
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password"""
