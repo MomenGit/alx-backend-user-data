6d1be6ca72621375a08dfe775e7128a3cea9c98f
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-22 13:06:16 +0200

    Implement login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Route Module for the API
 """
-from flask import Flask, jsonify, request
+from flask import Flask, Response, abort, jsonify, request
 from flask_cors import CORS
 from auth import Auth
 
@@ -26,6 +26,19 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError as err:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    """"""
+    email, password = request.form.get("email"), request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    res = Response()
+    res.set_cookie("session_id", session_id)
+
+    return jsonify({"email": email, "message": "logged in"})
 
 
 if __name__ == "__main__":
