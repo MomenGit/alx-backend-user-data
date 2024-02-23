ef3e92c94f145df3f31db25fe7335e00e7246933
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-23 17:14:29 +0200

    Fix update_password method to update reset_token to None

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,9 +97,10 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
         except Exception as err:
             raise ValueError
+        hashed_pwd = _hash_password(password)
 
         self._db.update_user(user.id,
-                             hashed_password=_hash_password(password))
+                             hashed_password=hashed_pwd, reset_token=None)
 
 
 def _hash_password(password: str) -> bytes:
